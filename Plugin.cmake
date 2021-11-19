b3bb21ef8b1e487a111f2f1c7c7136e143179e9b
Rasbats <miker@members.swis.net>
2021-11-19 08:55:38 +0000

    GLES2

--- Plugin.cmake
+++ Plugin.cmake
@@ -74,6 +74,11 @@ macro(late_init)
   endif ()
   
   add_definitions(-DocpnUSE_GL)
+  
+  if (QT_ANDROID)
+    add_definitions(-DUSE_ANDROID_GLES2)
+  endif ()
+  
 endmacro ()
 
 macro(add_plugin_libraries)
